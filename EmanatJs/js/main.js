266a88415f2ba799d2074e76a2b115b10e20e52a
Nicat <stablizator@gmail.com>
2018-05-20 19:48:45 +0400

    Add files via upload

---
+++ EmanatJs/js/main.js
@@ -0,0 +1,16 @@
+var img = document.createElement("img");
+ img.className = "logo"
+img.src = "img/emanat.png";
+
+
+ 
+document.body.appendChild(img)
+
+var img = document.createElement("img");
+img.className = "info"
+img.src = "img/info.png";
+
+
+
+document.body.appendChild(img)
+
